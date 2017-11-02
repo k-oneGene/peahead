8858ea597fbb11a80ca2fd21c40bf665971f7598
Jin Park <jinparkhan+git@googlemail.com>
2017-11-02 15:13:52 +0000

    Basic nav and self data added.

--- selfdata/views.py
+++ selfdata/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import TemplateView
+from django.urls import reverse
 
 # Create your views here.
+
+# def home(request):
+#     html = "<h1>Hello world</h1>"
+#     return HttpResponse(html)
+
+
+class Selfdata_main(TemplateView):
+    template_name =  "selfdata_main.html"
+    # def get(self, request, *args, **kwargs):
+    #     return render(request, )
