1713023aa7cfe2f9ef6d499c37a20254817ef1a2
JinPa <jinpark.han@gmail.com>
2017-12-04 11:12:54 +0000

    settings for hosting.

--- peahead/settings/production.py
+++ peahead/settings/production.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'peahead.urls'
@@ -129,7 +130,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATIC_URL = '/static/'
 # STATIC_DIRS = (os.path.join(BASE_DIR,  'static'),)
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 TEMPLATE_DIRS = (os.path.join(BASE_DIR,  'templates'),)
