088e4efd35617e81966e372247d740ded003f89e
JinPa <jinpark.han@gmail.com>
2017-12-04 18:40:57 +0000

    Using os.path for all reference to db.

--- selfdata/Report/util_daily_report.py
+++ selfdata/Report/util_daily_report.py
@@ -18,7 +18,6 @@ from peahead.settings import BASE_DIR
 
 # Config related
 
-#TODO: NEED TO CHANGE!!! FOUND THE ERROR!!!!!!!!!!
 file_loc = os.path.join(BASE_DIR, 'selfdata_01.db')
 # file_loc = r'D:\OneDrive\0 My Files\0 System\3 Workspace\PycharmProjects\1 Personal Host Git\1 SelfData\Cleaning\selfdata_01.db'
 
