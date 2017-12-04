407d7e75ad0e221bd08a8a9845b76b0732b0f20b
JinPa <jinpark.han@gmail.com>
2017-12-04 10:58:03 +0000

    settings for hosting.

--- peahead/settings/production.py
+++ peahead/settings/production.py
@@ -84,10 +84,10 @@ DATABASES = {
     }
 }
 
-import dj_database_url
-db_from_env = dj_database_url.config()
-DATABASES['default'].update(db_from_env)
-DATABASES['default']['CONN_MAX_AGE'] = 500
+# import dj_database_url
+# db_from_env = dj_database_url.config()
+# DATABASES['default'].update(db_from_env)
+# DATABASES['default']['CONN_MAX_AGE'] = 500
 
 
 # Password validation
