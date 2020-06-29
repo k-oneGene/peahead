49a2602036ec8c72b108ef4959ab3e169dd07c7c
Jin Park <jinpark.han@gmail.com>
2020-06-29 12:45:13 +0100

    Running locally on latest python

--- peahead/settings/__init__.py
+++ peahead/settings/__init__.py
@@ -7,16 +7,16 @@ try:
     ENV_IS_FOR = config('ENV_IS_FOR')
 
     if ENV_IS_FOR == 'local':
-        print('Local Development setting is used')
+        # print('Local Development setting is used')
         from .local import *
-    elif ENV_IS_FOR == 'staging':
-        print('Staging Setting is used.')
-        # from .staging import *
+    # elif ENV_IS_FOR == 'staging':
+    #     print('Staging Setting is used.')
+    #     # from .staging import *
     elif ENV_IS_FOR == 'production':
-        print('Production Setting is used.')
+        # print('Production Setting is used.')
         from .production import *
     else:
         print('No settings is found. Please check if .env exist')
 except UndefinedValueError:
-    print('CI setting is used')
+    print('Please create .env file (or legacy - CI setting is used)')
     # from .ci import *
